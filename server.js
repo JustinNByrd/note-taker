d3c2a93e63934090442483c90f52e83043888083
Justin Byrd <justin_byrd@hotmail.com>
2021-05-01 18:20:53 -0400

    Added server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+// check for env setting or use 8080
+const PORT = parseInt(process.env.PORT) || 8080;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, () => console.log(`Web server listening on port: ${PORT}`))
