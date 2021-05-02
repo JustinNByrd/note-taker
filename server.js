9a3e0861aa9c361a450ae81986fc199c670e3b2c
Justin Byrd <justin_byrd@hotmail.com>
2021-05-02 12:42:06 -0400

    Added handling for get /api/notes

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const path = require("path");
+const fs = require('fs');
+const { RSA_NO_PADDING } = require("constants");
+
 const app = express();
 
-// check for env setting or use 8080
+// check for env setting (heroku compatibility) or use 8080
 const PORT = parseInt(process.env.PORT) || 8080;
 
 app.use(express.static('./public'));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    let file = fs.readFileSync('./db/db.json');
+    let jsonData = JSON.parse(file);
+    res.send(jsonData);
+});
 
 app.listen(PORT, () => console.log(`Web server listening on port: ${PORT}`));
